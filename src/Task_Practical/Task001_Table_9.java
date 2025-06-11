f269781ef6ac554a188793e381b73187de677e87
Mangesh Panchwagh <panchwaghmangesh177@gmail.com>
2025-06-11 23:20:22 +0530

    10th June 2025

---
+++ src/Task_Practical/Task001_Table_9.java
@@ -0,0 +1,15 @@
+package Task_Practical;
+
+public class Task001_Table_9 {
+
+	public static void main(String[] args) {
+		int n = 9;
+		for (int i = 1; i <= 10; i++) {
+			
+			int multiply = 9 * i;
+			
+			System.out.printf("%d x %d = %d", n, i, multiply);
+			System.out.println();
+		}
+	}
+}
