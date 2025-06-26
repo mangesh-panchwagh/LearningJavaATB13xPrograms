9cb642a172650aa6e2440e624a86a9fa2672520e
Mangesh Panchwagh <panchwaghmangesh177@gmail.com>
2025-06-26 22:59:56 +0530

    11 June 2025

--- src/ex_04_Operators/Lab039_Operator_Modulus.java
+++ src/ex_04_Operators/Lab039_Operator_Modulus.java
@@ -3,6 +3,13 @@ package ex_04_Operators;
 public class Lab039_Operator_Modulus {
 
 	public static void main(String[] args) {
+		// < Less Than
+        // < =  -> Less than or equal to
+        // > Greater
+        // > = Greater or equal
+        // == ->  Equal to (but checking)
+        // != -> Not equal
+		
 		int a = 20;
 		int b = 10;
 		System.out.println(a%b);
---
+++ src/ex_04_Operators/Lab051_InstanceOf_Operator.java
@@ -0,0 +1,12 @@
+package ex_04_Operators;
+
+public class Lab051_InstanceOf_Operator {
+
+	public static void main(String[] args) {
+		
+		 String s1 = new String("Mangesh");
+         System.out.println(s1 instanceof String);
+         System.out.println(s1 instanceof Object);
+        // System.out.println(s1 instanceof Integer);
+	}
+}
