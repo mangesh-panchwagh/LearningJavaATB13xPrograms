68f722734f63fc570b54dc26b817fa8cca99ea33
Mangesh Panchwagh <panchwaghmangesh177@gmail.com>
2025-11-29 17:15:09 +0530

    1 July 2025

--- src/ex_17_OOP/Lab172_OOPs.java
+++ src/ex_17_OOP/Lab172_OOPs.java
@@ -4,7 +4,7 @@ public class Lab172_OOPs {
 
 }
 
-class Lab172 {
+class Lab173 {
 	public static void main(String[] args) {
 		
 		System.out.println("HI");
---
+++ src/ex_17_OOP/P9.java
@@ -0,0 +1,5 @@
+package ex_17_OOP;
+
+public class P9 {
+
+}
